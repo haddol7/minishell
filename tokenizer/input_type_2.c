f87f6fa401b72dab6be5407c2393053aff8921ed
Jeongsu0880 <latsyrc900@gmail.com>
2024-05-27 20:26:56 +0900

    Feat: First tokenizer complete version commit 첫번째 토크나이저 완성본 커밋

---
+++ tokenizer/input_type_2.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_type_2.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jungslee <jungslee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/27 20:12:40 by jungslee          #+#    #+#             */
+/*   Updated: 2024/05/27 20:13:03 by jungslee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tokenizer.h"
+
+void	input_paren(t_token **head, char input, int *idx)
+{
+	t_token_type	type;
+	char			*value;
+	t_token			*node;
+
+	if (input == '(')
+		type = T_LPAREN;
+	else
+		type = T_RPAREN;
+	value = (char *)malloc(sizeof(char) * 2);
+	if (value == NULL)
+		handle_error("exit : malloc error", 1, 0);
+	value[0] = input;
+	value[1] = '\0';
+	node = ms_lstnew(value, type);
+	ms_lstadd_back(head, node);
+	*idx = *idx + 1;
+}
+
+void	input_and_if(t_token **head, int *idx)
+{
+	t_token	*node;
+	char	*value;
+
+	value = ft_strdup("&&");
+	if (value == NULL)
+		handle_error("exit : malloc error", 1, 0);
+	node = ms_lstnew(value, T_AND_IF);
+	ms_lstadd_back(head, node);
+	*idx = *idx + 2;
+}
+
+void	input_or_if(t_token **head, int *idx)
+{
+	t_token	*node;
+	char	*value;
+
+	value = ft_strdup("||");
+	if (value == NULL)
+		handle_error("exit : malloc error", 1, 0);
+	node = ms_lstnew(value, T_AND_IF);
+	ms_lstadd_back(head, node);
+	*idx = *idx + 2;
+}
+
+void	input_word(t_token **head, char *input, int *idx)
+{
+	t_token	*node;
+	char	*value;
+	int		len;
+	int		start;
+
+	len = 1;
+	start = *idx;
+	while (input[start + len] != '\0' && \
+			input[start + len] != ' ' && input[start + len] != '|' && \
+			input[start + len] != '<' && input[start + len] != '>' && \
+			input[start + len] != '(' && input[start + len] != ')' && \
+			!(input[start + len] == '&' && input[start + len] == '&') && \
+			!(input[start + len] == '|' && input[start + len] == '|'))
+		len++;
+	value = (char *)malloc(sizeof(char) * (len + 1));
+	if (value == NULL)
+		handle_error("exit : malloc error", 1, 0);
+	ft_strlcpy(value, (const char *)(input + start), len + 1);
+	node = ms_lstnew(value, T_WORD);
+	ms_lstadd_back(head, node);
+	*idx = start + len;
+}
---
+++ tokenizer/tokenizer_utils.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jungslee <jungslee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/27 11:53:29 by jungslee          #+#    #+#             */
+/*   Updated: 2024/05/27 20:13:43 by jungslee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tokenizer.h"
+
+int	handle_error(char *str, int code, t_token **head)
+{
+	write(2, str, ft_strlen(str));
+	if (code == 1)
+		exit(1);
+	else
+	{
+		ms_free_all_token(head);
+		return (0);
+	}
+}
+
+t_token	*ms_lstnew(char *value, t_token_type type)
+{
+	t_token	*node;
+
+	node = (t_token *)malloc(sizeof(t_token));
+	if (node == NULL)
+		handle_error("exit : malloc error", 1, 0);
+	node->type = type;
+	node->value = value;
+	node->next = NULL;
+	return (node);
+}
+
+t_token	*ms_lstlast(t_token *head)
+{
+	t_token	*tmp;
+
+	tmp = head;
+	if (head == NULL)
+		return (NULL);
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	return (tmp);
+}
+
+void	ms_lstadd_back(t_token **head, t_token *new)
+{
+	t_token	*last;
+
+	if (head == NULL || new == NULL)
+		return ;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		last = ms_lstlast(*head);
+		last->next = new;
+	}
+}
+
+int	ms_free_all_token(t_token **head)
+{
+	t_token	*to_free;
+	t_token	*tmp;
+
+	to_free = *head;
+	while (to_free != NULL)
+	{
+		tmp = to_free->next;
+		if (to_free->type != T_EOF)
+			free(to_free->value);
+		free(to_free);
+		to_free = tmp;
+	}
+	return (0);
+}
