7dc5d1614aeaed2fd7791b628dd5ad7d7a84858b
haddol7 <haddol010313@khu.ac.kr>
2024-05-27 14:18:58 +0900

    Docs: Add BNF grammer BNF 문법 문서 추가. 공식 bash 및 minishell BNF

--- main.c
+++ main.c
@@ -1,1 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/25 18:27:00 by daeha             #+#    #+#             */
+/*   Updated: 2024/05/27 14:17:39 by daeha            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "minishell.h"
+
+#include <stdio.h>
+
+static int	g_status;
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_token	*token;
+	t_node	*ast;
+
+	token = tokenizer(argv[1]);
+	ast = parser(token);
+	return (0);
+}
