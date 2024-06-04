5ae5523078dbcf9d150fa3054e39c2a6debac722
haddol7 <haddol010313@khu.ac.kr>
2024-06-04 19:32:26 +0900

    Init: redir functions, files

---
+++ execution/redirection.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirection.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 19:21:20 by daeha             #+#    #+#             */
+/*   Updated: 2024/06/04 19:30:14 by daeha            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	input(char *filename)
+{
+	
+}
+
+void	append(char *filename)
+{
+
+}
+
+void	here_doc(char *delim)
+{
+
+}
+
+void	output(char *filename)
+{
+	
+}
---
+++ inc/execution.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/25 18:26:02 by daeha             #+#    #+#             */
+/*   Updated: 2024/06/04 19:32:14 by daeha            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXECUTION_H
+# define EXECUTION_H
+
+# include <fcntl.h>
+# include "minishell.h"
+
+void	input(char *filename);
+void	append(char *filename);
+void	here_doc(char *delim);
+void	output(char *filename);
+
+#endif
