032c278a588393f54fe8521afb8a5acfd0dfcfc7
haddol7 <haddol010313@khu.ac.kr>
2024-06-09 17:50:43 +0900

    Test: arg tester

--- test.sh
+++ test.sh
@@ -1,2 +1,16 @@
-#!/bin/bash
-echo "parameter count : $#"
+#!/bin/sh
+
+# 인자의 개수 출력
+echo "Number of arguments: $#"
+
+# 인자의 값 출력
+echo "Arguments: $@"
+
+# 개별 인자 출력
+count=1
+for arg in "$@"
+do
+    echo "Argument $count: $arg"
+    count=$((count + 1))
+done
+
