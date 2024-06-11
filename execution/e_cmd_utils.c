bce6b037e189e5f8c14f67c4b8acbbb28ee6bd37
haddol7 <haddol010313@khu.ac.kr>
2024-06-11 20:08:55 +0900

    Move: move error functions

--- execution/e_cmd_utils.c
+++ execution/e_cmd_utils.c
@@ -6,8 +6,16 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 17:10:55 by daeha             #+#    #+#             */
-/*   Updated: 2024/06/11 17:11:03 by daeha            ###   ########.fr       */
+/*   Updated: 2024/06/11 17:21:03 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
+
+void	exit_cmd_not_found(char *cmd)
+{
+	ft_putstr_fd("bash: ", STDERR_FILENO);
+	ft_putstr_fd(cmd, STDERR_FILENO);
+	ft_putendl_fd(": command not found", STDERR_FILENO);
+	exit(127);
+}
