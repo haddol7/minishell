03ca80a2d3b005b64851676ae3684a3d0c3292ee
haddol7 <haddol010313@khu.ac.kr>
2024-06-15 19:19:06 +0900

    Feat: Add env builtin

--- builtin/env.c
+++ builtin/env.c
@@ -6,14 +6,24 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 20:41:51 by daeha             #+#    #+#             */
-/*   Updated: 2024/06/12 22:31:44 by daeha            ###   ########.fr       */
+/*   Updated: 2024/06/15 19:05:11 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ms_env(char **arg, t_env *stat)
+void	ms_env(char **arg, t_env *env)
 {
-	if (arg != NULL || stat != NULL)
-		printf("ms_env func detected\n");
+	if (arg[1] != NULL)
+		error_cmd_exit(arg[1], ENOENT);
+	while (env)
+	{
+		if (env->complete == 0)
+			continue;
+		ft_putstr_fd(env->key, STDOUT_FILENO);
+		ft_putchar_fd('=', STDOUT_FILENO);
+		ft_putendl_fd(env->value, STDOUT_FILENO);
+		env = env->next;
+	}
+	exit(EXIT_SUCCESS);
 }
--- inc/builtin.h
+++ inc/builtin.h
@@ -6,7 +6,7 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 20:42:49 by daeha             #+#    #+#             */
-/*   Updated: 2024/06/15 18:44:33 by daeha            ###   ########.fr       */
+/*   Updated: 2024/06/15 19:03:32 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 
 # include "expansion.h"
 
-void	ms_cd(char **arg, t_env *stat);
-void	ms_env(char **arg, t_env *stat);
+void	ms_cd(char **arg, t_env *env);
+void	ms_env(char **arg, t_env *env);
 void	ms_pwd(void);
 void	ms_echo(char **arg);
-void	ms_exit(char **arg, t_env *stat);
-void	ms_unset(char **arg, t_env *stat);
-void	ms_export(char **arg, t_env *stat);
+void	ms_exit(char **arg, t_env *env);
+void	ms_unset(char **arg, t_env *env);
+void	ms_export(char **arg, t_env *env);
 
 #endif
