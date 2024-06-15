2e9d675cbbf6fd200b4998c06d02334bf5d247b4
haddol7 <haddol010313@khu.ac.kr>
2024-06-16 06:56:04 +0900

    Feat: temp exit function

--- builtin/exit.c
+++ builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 20:41:48 by daeha             #+#    #+#             */
-/*   Updated: 2024/06/15 21:32:43 by daeha            ###   ########.fr       */
+/*   Updated: 2024/06/16 06:55:46 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,24 @@ extern int g_status;
 
 void	ms_exit(char **arg, t_env *stat)
 {
-	if (arg != NULL || stat != NULL)
-		printf("ms_exit func detected\n");
-	g_status = EXIT_SUCCESS;
+	int	status;
+	
+	if (arg[1] == NULL)
+		exit(EXIT_SUCCESS);
+	else if (!is_valid_code(arg[1]))
+	{
+		ft_putendl_fd("minishell: numeric argument required", STDERR_FILENO);
+	}
+	else if (arg[2] != NULL)
+	{
+		ft_putendl_fd("minishell: exit: too many arguments", STDERR_FILENO);
+		g_status = EXIT_FAILURE;
+		return ;
+	}
+	status = ft_atoi(arg[1]);
+	if (status >= 0)
+		status = status % 256;
+	else
+		status = 256 + (status % 256);
+	exit(status);
 }
