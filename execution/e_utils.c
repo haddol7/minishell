240b62296e2004cdf3b98922dc041d76520f9656
haddol7 <haddol010313@khu.ac.kr>
2024-06-17 16:22:32 +0900

    Fix: way of waiting child processes

--- execution/e_utils.c
+++ execution/e_utils.c
@@ -6,7 +6,7 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 16:04:44 by daeha             #+#    #+#             */
-/*   Updated: 2024/06/15 20:58:42 by daeha            ###   ########.fr       */
+/*   Updated: 2024/06/17 16:22:25 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 extern int	g_status;
 
-//TODO: kill 함수로 다 죽여버릴까? 아님 기다릴까..
 void	push_pid_list(pid_t pid, t_stat *stat)
 {
 	if (stat->n_pid == MAX_PID - 1)
@@ -29,15 +28,20 @@ void	push_pid_list(pid_t pid, t_stat *stat)
 	stat->pid[stat->n_pid++] = pid;
 }
 
-//TODO: g_status 관리
 void	wait_pid_list(t_stat *stat)
-{
-	while (stat->n_pid >= 0)
+{	
+	int	pid;
+
+	pid = 0;
+	while (pid < stat->n_pid)
 	{
-		waitpid(stat->pid[--stat->n_pid], &g_status, 0);
-		if (WIFEXITED(g_status))
-			g_status = WEXITSTATUS(g_status);
+		waitpid(stat->pid[pid], &g_status, 0);
+		pid++;
 	}
+	if (WIFEXITED(g_status))
+		g_status = WEXITSTATUS(g_status);
+	else if (WIFSIGNALED(g_status))
+		g_status = WTERMSIG(g_status);
 }
 
 char	*env_find_value(char *key, t_env *envp)
