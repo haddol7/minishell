9b2d5232cf4dac04b7ccc173b831ed6e55d91e5e
haddol7 <haddol010313@khu.ac.kr>
2024-06-26 22:00:38 +0900

    Norm

--- execution/e_utils.c
+++ execution/e_utils.c
@@ -6,7 +6,7 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 16:04:44 by daeha             #+#    #+#             */
-/*   Updated: 2024/06/26 21:24:32 by daeha            ###   ########.fr       */
+/*   Updated: 2024/06/26 22:00:26 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,7 @@ void	wait_pid_list(t_stat *stat)
 		write(1, "\n", 1);
 	}
 }
+
 void	del_here_doc_tmp_file(t_node *node)
 {
 	if (node == NULL || node->type == N_CMD)
