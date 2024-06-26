34f39e96b2dae34deffd9e56ad401f08f1695e3f
haddol7 <haddol010313@khu.ac.kr>
2024-06-26 17:31:20 +0900

    Fix: #44

--- execution/e_and_or_if.c
+++ execution/e_and_or_if.c
@@ -6,31 +6,54 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 21:02:49 by daeha             #+#    #+#             */
-/*   Updated: 2024/06/25 02:53:48 by daeha            ###   ########.fr       */
+/*   Updated: 2024/06/26 17:28:15 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "execution.h"
 
+static void	save_pre_cmd_fd(int origin[2], t_stat *stat);
+static void	restore_pre_cmd_fd(int origin[2], t_stat *stat);
+
 void	exec_and_or_if(t_node *node, t_stat *stat)
 {
 	int	*status;
 	int	origin[2];
 
-	origin[INPUT] = dup(stat->fd[INPUT]);
-	origin[OUTPUT] = dup(stat->fd[OUTPUT]);
-	push_pipe_list(origin[INPUT], stat);
-	push_pipe_list(origin[OUTPUT], stat);
+	save_pre_cmd_fd(origin, stat);
 	execution(node->left, stat);
 	wait_pid_list(stat);
+	restore_pre_cmd_fd(origin, stat);
 	status = get_status();
-	dup2(origin[INPUT], stat->fd[INPUT]);
-	close(origin[INPUT]);
-	dup2(origin[OUTPUT], stat->fd[OUTPUT]);
-	close(origin[OUTPUT]);
 	if (node->type == N_AND && *status == EXIT_SUCCESS)
 		execution(node->right, stat);
 	else if (node->type == N_OR && *status != EXIT_SUCCESS)
 		execution(node->right, stat);
+}
+
+static void	save_pre_cmd_fd(int origin[2], t_stat *stat)
+{
+	origin[INPUT] = dup(stat->fd[INPUT]);
+	origin[OUTPUT] = dup(stat->fd[OUTPUT]);
+	push_pipe_list(origin[INPUT], stat);
+	push_pipe_list(origin[OUTPUT], stat);
+}
+
+static void	restore_pre_cmd_fd(int origin[2], t_stat *stat)
+{
+	if (stat->fd[INPUT] == -1)
+		stat->fd[INPUT] = origin[INPUT];
+	else
+	{
+		dup2(origin[INPUT], stat->fd[INPUT]);
+		close(origin[INPUT]);
+	}
+	if (stat->fd[OUTPUT] == -1)
+		stat->fd[OUTPUT] = origin[OUTPUT];
+	else
+	{
+		dup2(origin[OUTPUT], stat->fd[OUTPUT]);
+		close(origin[OUTPUT]);
+	}
 }
